cb4fa786b76998e7177ed06e24e39fe2a4c9e297
Sanjit-K-Subba <sanjeevlimbu@gmail.com>
2026-03-29 12:08:34 +0800

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,34 @@
 
 console.log( 3 + 3);
 console.log ("Sanjit")// code readability should be high//something like this//
+
+//we can use an extension called (prettier) to indent the code.
+
+
+//Documentations ( we use MDN, its from Mozilla)//
+//Orginal or Standard documentation is found in https://tc39.es/ecma262/ (ECMAScript)//
+//ecma international is the body who standerdise the JavaScriopt//
+
+//DataTyapes eg.
+
+let name = "Sanjit" //String
+let age = 18 //Number
+let isLogIn = false //Boolean (Yes or No)
+
+//DataTyapes
+
+//string =>"" (can use single code or double code)
+//number => numbers.. number has a range 2 to the power of 53, can assinged
+//                    if have to store more than this number we use
+//bigint => if you have lots of numbers
+//boolean => is a true or false
+//null => is a datatype and also a standalone value, it doesnt mean there's no value
+//        it means have something but dont have value its like empty  
+//undefined => the one which we haven't defined the value
+//symbol => we use this to identify unique components
+
+//object => 
+
+console.log(typeof "Sanjit");
+console.log(typeof undefined);//undefined (if you run, it shows undefined)
+console.log(typeof null);//object (if you run, it shows objedt)
