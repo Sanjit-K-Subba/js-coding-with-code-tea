a2634cde8c6e5e4296572e79755461f8d7ec050c
Sanjit-K-Subba <sanjeevlimbu@gmail.com>
2026-03-29 01:46:26 +0000

    datatyapes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 12345;
+let accountEmail = "sanjeevlimbu@gmail.com";
+var accountPassword = "54321";
+accountCity = "Kathmandu"; //this way also can create var, but never to use//
+let accountState; //variable is not defined here//
+
+// accountID = 56789;, Constant is not changble.//
+//const is always use (const)//
+/* to set the variables we use (let) not (var) i mean can use but better we use let */
+// prefer not to use var//
+//becuz issue in block scope and functional scope//
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
