9074b2067a90f50261af79011c782bf8078499d9
Sanjit-K-Subba <sanjeevlimbu@gmail.com>
2026-03-29 12:02:13 +0800

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,5 +1,7 @@
 "use strict"; // this makes js into newer version or maybe in new js no need to use //
+
 //alert( 3 + 3 ); this code is not runs her because we are using nodejs, not browser//
-//but if you run this code is to browser console it will show up the result//
+//yeah, if you run this code is to browser console it will show up the result//
+
 console.log( 3 + 3);
 console.log ("Sanjit")// code readability should be high//something like this//
